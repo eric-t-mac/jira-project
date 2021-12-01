e933c6284fe97b572d6185a7dfb52ab4606e6430
v_zekaikang <v_zekaikang@tencent.com>
2021-12-01 18:05:45 +0800

    fix: refresh

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,3 +1,9 @@
+import { SearchPanel } from "./search-panel"
+import { List } from "./list"
+
 export const ProjectListScreen = () => {
-    return <div></div>
+    return <div>
+        <SearchPanel/>
+        <List/>
+    </div>
 }
--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+
 export const SearchPanel = () => {
-    return <form action=""></form>
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const [users, setUsers] = useState([])
+
+    return (
+        <form action="">
+            <div>
+                <input type="text" value={param.name} onChange={evt => 
+                    // setParam(Object.assign({}, param, {name:evt?.target?.value}))
+                    setParam({
+                        ...param,
+                        name: evt?.target?.value
+                    })}
+                />
+                <select value={param.personId} onChange={evt => 
+                    setParam({
+                        ...param,
+                        personId: evt?.target?.value
+                    })}
+                >
+                    <option value={""}>负责人</option>
+                    {
+                        users?.map(user => 
+                            <option value={user?.id}>
+                                {user?.name}
+                            </option>
+                        )
+                    }
+                </select>
+            </div>
+        </form>
+    )
 }
