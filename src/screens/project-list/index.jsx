a5c8695f6debe3486174861504a24f334cd2b868
v_zekaikang <v_zekaikang@tencent.com>
2021-12-07 10:50:15 +0800

    fix: 添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 import * as qs from "qs";
 
 // npm start 访问.env.development npm run build 访问.env
@@ -17,20 +17,22 @@ export const ProjectListScreen = () => {
     const [list, setList] = useState([]);
     const [users, setUsers] = useState([]); 
 
+    const debouncedParam = useDebounce(param, 2000);
+
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json());
             }
         })
-    }, [param])
-    useEffect(() => {
+    }, [debouncedParam])
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json());
             }
         })
-    }, [])
+    })
     console.log(">>>projects", list);
     console.log(">>>users", users);
     console.log(">>>param", param);
