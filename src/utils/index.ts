f4dd88be4d584654446068fd73945a5ecf52e80f
v_zekaikang <v_zekaikang@tencent.com>
2021-12-20 15:18:29 +0800

    chore: any -> unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: object) => {
@@ -25,7 +25,7 @@ export const useMount = (callback: () => void) => {
     }, [])
 }
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = (value: unknown, delay?: number): any => {
     // let timer;
     // return (...param) => {
     //     if(timer) {
